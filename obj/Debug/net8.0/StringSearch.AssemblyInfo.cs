6e2239252b85ad352629f7eaf0cb02ed6764546a
Leonardo Heinen Oliveira <108004374+leo-oliveira2@users.noreply.github.com>
2024-10-03 14:26:38 -0300

    Config files

--- obj/Debug/net8.0/StringSearch.AssemblyInfo.cs
+++ obj/Debug/net8.0/StringSearch.AssemblyInfo.cs
@@ -10,13 +10,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StringSearch")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4367160c79270854e0313c14194dbbd2640c00ef")]
-[assembly: System.Reflection.AssemblyProductAttribute("StringSearch")]
-[assembly: System.Reflection.AssemblyTitleAttribute("StringSearch")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+// [assembly: System.Reflection.AssemblyCompanyAttribute("StringSearch")]
+// [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+// [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+// [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4367160c79270854e0313c14194dbbd2640c00ef")]
+// [assembly: System.Reflection.AssemblyProductAttribute("StringSearch")]
+// [assembly: System.Reflection.AssemblyTitleAttribute("StringSearch")]
+// [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
